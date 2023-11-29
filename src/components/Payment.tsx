b3faa33ad25f54fa50d6cea5ca63a55ee5d29dac
sojeongwan <jeongs9203@naaver.com>
2023-11-30 08:53:10 +0900

    fix: AI분석중에 로딩

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import {
     PaymentWidgetInstance,
     loadPaymentWidget,
